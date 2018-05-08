69162d5994084630550f5853c6a06afb2657651e
Alex Mitrakhovich <rem.lex@mail.ru>
2018-05-08 23:54:54 +0300

    Update 2017_12_25_181423_create_files_table.php
    Fix minor syntax bug

--- database/migrations/2017_12_25_181423_create_files_table.php
+++ database/migrations/2017_12_25_181423_create_files_table.php
@@ -18,7 +18,7 @@ class CreateFilesTable extends Migration
             $table->string('name')->unique();
             $table->string('type');
             $table->string('extension');
-            $table->integer('user_id')->unsigned();;
+            $table->integer('user_id')->unsigned();
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
