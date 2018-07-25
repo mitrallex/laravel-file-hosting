5fddaf353fd8dd6596f8bae91b0be57179fd2529
Alex Mitrakhovich <rem.lex@mail.ru>
2018-07-25 14:31:57 +0300

    Fix error with loading image in hidden modal window

--- resources/views/layouts/modal.blade.php
+++ resources/views/layouts/modal.blade.php
@@ -2,7 +2,7 @@
     <div class="modal-background" @click="closeModal()"></div>
         <div class="modal-content">
             <p class="image is-4by3">
-                <img src=""  :src="'{{ asset('storage/' . Auth::user()->name . '_' . Auth::id()) }}' + '/' + file.type + '/' + file.name + '.' + file.extension" :alt="file.name">
+                <img v-if="Object.keys(file).length !== 0" src=""  :src="'{{ asset('storage/' . Auth::user()->name . '_' . Auth::id()) }}' + '/' + file.type + '/' + file.name + '.' + file.extension" :alt="file.name">
             </p>
         </div>
     <button class="modal-close is-large" aria-label="close" @click="closeModal()"></button>
